643cf1467b15528d787edcfa7aaef1fe6b903f21
Anatolii Ostapenko <tec303@gmail.com>
2022-01-19 18:22:25 +0200

    public repo

---
+++ src/hw1.java
@@ -0,0 +1,20 @@
+public class hw1 {
+    public static void main(String[] args) {
+        //System.out.println("anatolii ostapenko");
+//        int name = 8;
+//
+//        if( name <= 7 ){
+//            System.out.print("more than 7");
+//        }else{
+//            System.out.print("less than 7");
+//        }
+
+        for(int x = 5, y=0; y <=10; x = x+2, y++) {
+            System.out.print("Значение x: " + x );
+            System.out.print("\n");
+        }
+
+
+
+    }
+}
---
+++ src/hw2/Hw2.java
@@ -0,0 +1,8 @@
+package hw2;
+
+public class Hw2 {
+    public static void main(String[] args) {
+
+
+    }
+}
